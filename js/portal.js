8fb83651b0b75f101b0c0bdab118caafcb052046
Jeanbede <njeanbeda@gmail.com>
2021-01-13 02:27:51 +0300

    adding layout to html form

--- js/portal.js
+++ js/portal.js
@@ -1,29 +1,30 @@
-function results(){
-    var fname=document.getElementById('inputfname4').value;
-    var lname=document.getElementById('inputlname4').value;
-    var address=document.getElementById('input-address').value;
-    var addresstwo=document.getElementById('input-addresstwo').value;
-    var city=document.getElementById('location').value;
-    var status=document.getElementById('input-status').value;
-    var comment=document.getElementById('input-comment').value;
-    
-    document.write(fname+"br/>");
-    document.write(lname+"br/>");
-    document.write(address+"br/>");
-    document.write(addresstwo+"br/>");
-    document.write(city+"br/>");
-    document.write(status+"br/>");
-    document.write(comment+"br/>");
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+//   this.addressMail= address;
+//   this.phoneNumber= phone;
+}
 
-
-    document.getElementById("submit") .innerHTML="Your Are Added as Member : " + fname+lname;
-
-};
-
-
-$(".container").submit(function(event) {
+// user interface logic
+$(document).ready(function() {
+  $("#form.container").submit(function(event) {
     event.preventDefault();
-    var name = $("#naame").val();
-    alert("Our Dear Client," + name+ ".We have received your  Order, It will delivered to your doorstep shortly;Thank you for Choosing us!!!");
-    document.getElementById("my-form").reset();
-});
+
+    var inputtedFirstName = $("input#inputfname").val();
+    var inputtedLastName = $("input#inputlname").val();
+    // var inputtedAddressMail=$("input#new-email").val();
+    // var inputtedPhoneNo=$("input#new-phone").val();
+
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    // inputtedAddressMail,inputtedPhoneNo
+
+    $("ul#members").append("<li><span class='members'>" + newContact.firstName +newContact.lastName+"</span></li>");
+    // newContact.addressMail+newContact.phoneNo+
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    // $("input#new-email").val("");
+    // $("input#new-phone").val("");
+  });
+}); 
