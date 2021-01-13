031cc23dcb26b13e13dd76d6a66afd0487a3db1f
Vincent-Juma <vincentjuma64@gmail.com>
2021-01-13 14:30:53 +0300

    add stylings and update target selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // SERVICES SCRIPT
-alert("welcome vj");
+// alert("welcome vj");
 
 var mission_img = '<img src="./img/about.jpeg" height="250px" width="350px"><br><h5>MISSION</h5>';
 var mission_text2 = '<h5>MISSION</h5><p>We create wealth for members by mobilization of savings and provision of competitive loans, while assuring high returns through innovative financial solutions</p>';
 $(document).ready(function()
 {
-  $("#mission").hover(function()
+  $("#mission").click(function()
   {
       $(this).html($(this).html() == mission_text2 ? mission_img : mission_text2);
   });
@@ -15,7 +15,7 @@ var vision_img = '<img src="./img/sacco.jpg" height="250px" width="350px"><br><h
 var vision_text2 = '<h5>MISSION</h5><p>To be the preferred financial services provider to our members</p>';
 $(document).ready(function()
 {
-  $("#vision").hover(function()
+  $("#vision").click(function()
   {
       $(this).html($(this).html() == vision_text2 ? vision_img : vision_text2);
   });
@@ -24,7 +24,7 @@ var motto_img = '<img src="./img/sacco3.jpg" height="250px" width="350px"><br><h
 var motto_text2 = '<h5>MISSION</h5><p>Financial management is the the key to success.</p>';
 $(document).ready(function()
 {
-  $("#motto").hover(function()
+  $("#motto").click(function()
   {
       $(this).html($(this).html() == motto_text2 ? motto_img : motto_text2);
   });
