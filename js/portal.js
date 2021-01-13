2b830d9ce48a056e32fd47aa60fa2ba618651f24
Jeanbede <njeanbeda@gmail.com>
2021-01-13 03:13:10 +0300

    adding commandson js

--- js/portal.js
+++ js/portal.js
@@ -1,30 +1,22 @@
+
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-//   this.addressMail= address;
-//   this.phoneNumber= phone;
 }
 
-// user interface logic
 $(document).ready(function() {
-  $("#form.container").submit(function(event) {
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#inputfname").val();
-    var inputtedLastName = $("input#inputlname").val();
-    // var inputtedAddressMail=$("input#new-email").val();
-    // var inputtedPhoneNo=$("input#new-phone").val();
-
-
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+  
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    // inputtedAddressMail,inputtedPhoneNo
-
-    $("ul#members").append("<li><span class='members'>" + newContact.firstName +newContact.lastName+"</span></li>");
-    // newContact.addressMail+newContact.phoneNo+
-
+   
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName +newContact.lastName+"</span></li>");
+   
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    // $("input#new-email").val("");
-    // $("input#new-phone").val("");
+   
   });
 }); 
