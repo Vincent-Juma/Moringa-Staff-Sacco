ac3ead3dbb9e4b227878f51843ca5cfe1519478a
Jeanbede <njeanbeda@gmail.com>
2021-01-14 19:01:27 +0300

    adding style to form

--- js/portal.js
+++ js/portal.js
@@ -1,22 +1,41 @@
 
-function Contact(first, last) {
+function Contact(first, last,email,telephone,city,gender) {
   this.firstName = first;
   this.lastName = last;
+  this.emailAddress=email;
+  this.telephoneNo=telephone;
+  this.residentCity=city;
+  this.statusGender=gender;
 }
 
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+$(document).ready(function () {
+  $("form#new-contact").submit(function (event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-  
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
-   
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName +newContact.lastName+"</span></li>");
-   
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-   
+    var inputtedEmailAddress=$("input#new-email").val();
+
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmailAddress);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + newContact.lastName +newContact.emailAddress+ "</span></li>");
+    // $("#contacts").append("<li>" + newContact.firstName+ newContact.lastName + newContact.emailAddress+ "</li>");
+
+    $("#new-first-name").val("");
+    $("#new-last-name").val("");
+    $("#new-email").val("");
+    alert('You Have Sucessfully Registered as A Sacco Member!!!')
+
   });
-}); 
+  
+});
+
+
+
+
+
+
+
+
+
