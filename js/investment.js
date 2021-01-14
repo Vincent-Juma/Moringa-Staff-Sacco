acc1ebd47b89ea7236ae64768f53f2e19022acba
Harry Bett <harrybett89@gmail.com>
2021-01-14 05:39:30 +0300

    add constructor function

---
+++ js/investment.js
@@ -0,0 +1,21 @@
+let amount, interestRate;
+let total = 0;
+
+class Gettotal {
+  constructor(package, investment, time, total) {
+    this.package = package;
+    this.investment = investment;
+    this.time = time;
+    this.total = total;
+  }
+}
+
+
+
+
+
+
+
+$(document).ready(function () {
+  $('button.clear').trigger("reset")
+});
