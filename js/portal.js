6af7a48f016d20ba8d1c0f1d1f363e496a37d5a2
Jeanbede <njeanbeda@gmail.com>
2021-01-15 10:58:33 +0300

    adding files

--- js/portal.js
+++ js/portal.js
@@ -6,7 +6,7 @@ function Contact(first, last,email,telephone,city,gender) {
   this.telephoneNo=telephone;
   this.residentCity=city;
   this.statusGender=gender;
-}
+  }
 
 $(document).ready(function () {
   $("form#new-contact").submit(function (event) {
@@ -14,17 +14,15 @@ $(document).ready(function () {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedEmailAddress=$("input#new-email").val();
+   
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + newContact.lastName + "</span></li>");
 
-
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmailAddress);
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + newContact.lastName +newContact.emailAddress+ "</span></li>");
-    // $("#contacts").append("<li>" + newContact.firstName+ newContact.lastName + newContact.emailAddress+ "</li>");
 
     $("#new-first-name").val("");
     $("#new-last-name").val("");
-    $("#new-email").val("");
+    
     alert('You Have Sucessfully Registered as A Sacco Member!!!')
 
   });
